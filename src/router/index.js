90070c88a5c95f0c6cadf645cdbb8b6ebbe4bdf3
Jakkapong Praditthanachot <60100193+jakkapongqcue@users.noreply.github.com>
2026-08-21 09:31:34 +0700

    Update: Mount base path name

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(document.querySelector('base')?.getAttribute('href') || '/'),
   routes: [
     {
       path: '/',
